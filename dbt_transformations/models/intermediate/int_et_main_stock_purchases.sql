c7638931c5b571c33c2588fa91f5b809bf10949a
Tarmo <tsillajoe@gmail.com>
2025-07-13 10:18:22 +0300

    int_et_main_stock_purchases.sql

--- dbt_transformations/models/intermediate/int_et_main_stock_purchases.sql
+++ dbt_transformations/models/intermediate/int_et_main_stock_purchases.sql
@@ -12,11 +12,13 @@ with purchases as (
         history.firma_id,
         history.jrk,
         lmmat.nimi as nimetus,
-        cvgroups.grupinimi
+        replace(lmmat.tkood, ' ', '') as supplier_article,
+        cvgroups.grupinimi,
+        lmmat.kamark
         from {{ ref('stg_bao__sales_and_purchases') }} history
         left join {{ ref('stg_bao__rvsoft_products') }} lmmat using (siffer)
         left join {{ ref('rvsoft_cv_groups') }} cvgroups using (grupp)
-        where history.liik = 'O' and history.firma_id not in ('MEIE') and history.firma_id not ilike 'E80%'
+        where history.liik = 'O' and history.firma_id not in ('MEIE') and history.firma_id not ilike 'E80%' and grupinimi is not null
             and history.kuup between '2017-01-01' and '2025-12-31'
 )
 select * from purchases
