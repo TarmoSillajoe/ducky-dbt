d2b5f649d64b5dbe170a48c44607b89a580784e6
Tarmo Sillajõe <tarmos@autoparts.local>
2025-07-16 15:21:18 +0300

    In analyses, exploring sales

--- dbt_transformations/analyses/explore.sql
+++ dbt_transformations/analyses/explore.sql
@@ -3,26 +3,24 @@ attach '{{ env_var('HOME') }}/ducky-dbt/data/data.duckdb';
 
 
 
-create or replace view purch as
-with purch as (
-    select * from {{ ref('int_et_main_stock_purchases') }}
-)
+-- create or replace view purch as
+-- with purch as (
+--     select * from {{ ref('int_et_main_stock_purchases') }}
+-- )
 
-select *
-from purch;
+-- select *
+-- from purch;
 
-
-
-
-with years as (
-    pivot purch
-    on date_part('year',date_purchased)
-    using try_cast(sum(purchase_qty_eur) as integer)
-    group by brand
-    )
-select * from years order by "2024" desc
+select
+    customer,
+    date_part('year', kuup) as year,
+    try_cast(sum(vsumma) as integer) as turnover,
+    try_cast(sum(vsumma) as integer) as quantity_eur,
+    try_cast((avg((vsumma-voma)/voma) * 100) as integer) as profitability,
+from {{ ref('int_et_main_stock_sales') }}
+group by customer, year
+order by year desc
 ;
 
-select max(kuup) from {{ ref('stg_bao__sales_and_purchases') }};
 detach data;
 
--- dbt_transformations/models/intermediate/int_et_main_stock_sales.sql
+++ dbt_transformations/models/intermediate/int_et_main_stock_sales.sql
@@ -10,14 +10,39 @@ with sales as (
         history.vkogus,
         history.firmanimi,
         history.firma_id,
-        history.jrk,
         history.voma,
         lmmat.nimi as nimetus,
-        cvgroups.grupinimi
+        coalesce(cvgroups.grupinimi, 
+                    bosch_cv.arbitrary_brand, 
+                    hengst_cv.arbitrary_brand,
+                    mann_cv.arbitrary_brand) as cv_group_name,
+
         from {{ ref('stg_bao__sales_and_purchases') }} history
         left join {{ ref('stg_bao__rvsoft_products') }} lmmat using (siffer)
         left join {{ ref('rvsoft_cv_groups') }} cvgroups using (grupp)
+        left join {{ ref('stg_bosch__cv_products') }} bosch_cv using(siffer)
+        left join {{ ref('stg_hengst__products') }} hengst_cv on lmmat.tkood_normalized=hengst_cv.code
+        left join {{ ref('stg_mann__products') }} mann_cv on lmmat.tkood_normalized=mann_cv.code
+        left join {{ ref('stg_fi__stocks') }} fi on history.siffer=fi.mekofi_code
+
+
         where history.liik = 'M' and history.firma_id not in ('MEIE') and history.firma_id not ilike 'E80%'
-            and history.kuup between '2017-01-01' and today()
+            and history.kuup between '2023-01-01' and '2025-12-31'
 )
-select * from sales
+,
+
+    final as (
+        select
+            firmanimi as customer,
+            firma_id,
+            vsumma,
+            vkogus,
+            voma,
+            kuup
+
+        from sales
+        where cv_group_name is not null
+
+    )
+
+    from final
