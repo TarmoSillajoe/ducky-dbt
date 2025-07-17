3a7627bca2deec7998dd005efcecfafd1691ec88
Tarmo Sillajõe <tarmos@autoparts.local>
2025-07-17 11:07:02 +0300

    Customer categories data added in seeds

--- dbt_transformations/models/intermediate/int_et_main_stock_sales_grouped.sql
+++ dbt_transformations/models/intermediate/int_et_main_stock_sales_grouped.sql
@@ -11,12 +11,14 @@ with grouped_by_customer_id as (
         firma_id,
         date_part('year', kuup) as year,
         try_cast(sum(vsumma) as integer) as quantity_eur,
-        round(avg((vsumma-voma)/voma), 2) as profitability,
+        cast(avg((vsumma-voma)/voma) filter (voma > 0) * 100 as integer) as avg_profitability,
+        --cast(median((vsumma-voma)/voma) filter (voma > 0) * 100 as integer) as med_profitability,
+ 
     from {{ ref('int_et_main_stock_sales') }} history
     
     group by firma_id, year
     order by year desc
 )
 
-
 select * from grouped_by_customer_id
+
--- dbt_transformations/models/intermediate/int_et_main_stock_sales_years_pivoted.sql
+++ dbt_transformations/models/intermediate/int_et_main_stock_sales_years_pivoted.sql
@@ -1,24 +1,34 @@
 
+
 with 
     years_pivoted as (
         pivot {{ ref('int_et_main_stock_sales_grouped') }}
         on year in (2023, 2024, 2025)
-        using first(quantity_eur) as quantity_eur, first(profitability) as avg_profitability
+        using first(quantity_eur) as quantity_eur, first(avg_profitability) as avg_profitability_percent
     ),
 
     names_and_ids as (
         select 
             years_pivoted.*,
             companies.nimi as name,
+            category_description,
             companies.kood
         from years_pivoted 
         join {{ ref('stg_bao__rvsoft_firma') }} companies on years_pivoted.firma_id=companies.kood
+        left join {{ ref('rvsoft_company_categories') }} cust_cats on companies.tyyp=cust_cats.category_id
     ),
 
     final as (
         select 
-            *
+            name,
+            category_description,
+            "2023_quantity_eur",
+            "2023_avg_profitability_percent",
+            "2024_quantity_eur",
+            "2024_avg_profitability_percent",
+            "2025_quantity_eur",
+            "2025_avg_profitability_percent"
         from names_and_ids
-        order by "2025_quantity_eur" desc
+        order by "2024_quantity_eur" desc
     )
 from final
