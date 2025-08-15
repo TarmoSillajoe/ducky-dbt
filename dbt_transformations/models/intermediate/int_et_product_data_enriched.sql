6337bf116f24284050c6d642702d9884ba32bd9b
Tarmo <tsillajoe@gmail.com>
2025-08-15 20:13:33 +0300

    stg_hengst_products.sql - columns names written out

--- dbt_transformations/models/intermediate/int_et_product_data_enriched.sql
+++ dbt_transformations/models/intermediate/int_et_product_data_enriched.sql
@@ -1,9 +1,18 @@
 with products as (
     SELECT
-        p.siffer,
-        p.grupp,
-        p.tkood
-    from {{ ref('stg_bao__rvsoft_products') }}
+        products.siffer,
+        products.tkood,
+        products.grupp,
+        cvgroups.grupinimi,
+        bosch_cv.arbitrary_brand,
+        hengst_cv.arbitrary_brand,
+    from {{ ref('stg_bao__rvsoft_products') }} products
+    left join {{ ref('rvsoft_cv_groups') }} cvgroups using (grupp)
+    left join {{ ref('stg_bosch__cv_products') }} bosch_cv using(siffer)
+    left join {{ ref('stg_hengst__products') }} hengst_cv on products.tkood_normalized=hengst_cv.code
+    left join {{ ref('stg_mann__products') }} mann_cv on products.tkood_normalized=mann_cv.code
+
+
 )
 
 select * from products
--- dbt_transformations/models/staging/hengst/stg_hengst__products.sql
+++ dbt_transformations/models/staging/hengst/stg_hengst__products.sql
@@ -1,10 +1,21 @@
 with final as (
     select
-        *,
+        edv,
+        status_country_exclusion,
+        hengsttype,
+        filter_type,
+        cv_buses,
+        pc_transporter,
+        offhighway,
         upper(
             regexp_replace(hengsttype, '\s|[.]|[-/]', '', 'ig')
         ) as code,
-        'hengst_cv' as arbitrary_brand
+        case
+            when cv_buses ilike '%x%' then 'hengst_cv'
+            when pc_transporter ilike '%x%' then 'hengst_pv'
+            when offhighway ilike '%x%' then 'hengst_offhighway'
+            else null
+        end as arbitrary_brand
     from
         read_xlsx(
             {{ source('hengst_products', 'stg_hengst__products') }},
@@ -12,8 +23,6 @@ with final as (
             normalize_names = true,
             stop_at_empty = true
         )
-    where
-        cv_buses ilike '%x%'
 )
 
 select *
